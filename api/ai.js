0714a3a478d347a77f0ba5d4d4dca7f67da12f39
coopjomoncar-spec <coopjomoncar@gmail.com>
2026-04-14 15:48:22 +0200

    ai.js

--- api/ai.js
+++ api/ai.js
@@ -1,4 +1,5 @@
 export default async function handler(req, res) {
+  // 🔥 CORS (ESTO ARREGLA TU ERROR)
   res.setHeader('Access-Control-Allow-Origin', 'https://coopjomoncar-spec.github.io');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
